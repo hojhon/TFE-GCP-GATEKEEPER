f4af843b6a9f28275786c7ac3c0a7413d9c7a978
hojhon <115334790+hojhon@users.noreply.github.com>
2026-01-03 02:01:14 -0500

    Remove workload identity binding from pipeline

--- main.tf
+++ main.tf
@@ -1,14 +1,13 @@
 #Organization Policy Constraints
-module "org_constraints" {
-  source = "./org-policy-constraints"
-  organization_id = var.organization_id
-}
+# module "org_constraints" {
+#   source = "./org-policy-constraints"
+#   organization_id = var.organization_id
+# }
 
 # Workload Identity for GitHub Actions  
 module "workload_identity" {
-  source             = "./workload-identity"
-  project_id         = var.project_id
-  service_account_id = "projects/${var.project_id}/serviceAccounts/tfe-gatekeeper@${var.project_id}.iam.gserviceaccount.com"
-  github_repository  = var.github_repository
-  github_owner       = var.github_owner
+  source            = "./workload-identity"
+  project_id        = var.project_id
+  github_repository = var.github_repository
+  github_owner      = var.github_owner
 }
